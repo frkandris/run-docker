5178d045c3fae907a679fd5130e4ad48eca810ec
frkandris <p.toth.andras@gmail.com>
2022-03-22 13:24:17 +0100

    removing wp, adding new examples
    removing wp, adding new examples

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ var Dockerode = require('dockerode');
 var DockerodeCompose = require('dockerode-compose');
 
 var docker = new Dockerode();
-var compose = new DockerodeCompose(docker, './wordpress/docker-compose.yml', 'wordpress');
+var compose = new DockerodeCompose(docker, './hello_python_from_node/docker-compose.yml', 'python');
 
 (async () => {
-  await compose.pull();
-  var state = await compose.up();
-  console.log(state);
+  console.time('pull');
+    await compose.pull();
+  console.timeEnd('pull');
+
+  console.time('up');
+    var state = await compose.up();
+    // console.log(state);
+  console.timeEnd('up');
+
 })();
